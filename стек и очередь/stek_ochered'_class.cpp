208916bc3071566e128c575b3fc2b9661dad61cf
Lihachev Danil <62751553+lihach53@users.noreply.github.com>
2023-06-01 01:05:05 +0500

    Add files via upload

---
+++ стек и очередь/stek_ochered'_class.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+
+#define SIZE 10
+#define _SIZE 1000
+
+using namespace std;
+
+class Queue
+{
+private:
+    int* arr;
+    int capacity;
+    int front;
+    int rear;
+    int count;
+public:
+    Queue(int size = _SIZE);
+    ~Queue();
+    
+    int dequeue();
+    void enqueue(int x);
+    int peek();
+    int size();
+    bool isEmpty();
+    bool isFull();
+
+    int* getArr() const { return arr; }
+    int getCapacity() const { return capacity; }
+    int getFront() const { return front; }
+    int getRear() const { return rear; }
+    int getCount() const { return count; }
+};
+
+Queue::Queue(int size)
+{
+    arr = new int[size];
+    capacity = size;
+    front = 0;
+    rear = -1;
+    count = 0;
+}
+
+Queue::~Queue() {
+    delete[] arr;
+}
+
+int Queue::dequeue()
+{
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+
+    int x = arr[front];
+    cout << "Removing " << x << endl;
+
+    front = (front + 1) % capacity;
+    count--;
+
+    return x;
+}
+
+void Queue::enqueue(int item)
+{
+    if (isFull())
+    {
+        cout << "Overflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Inserting " << item << endl;
+
+    rear = (rear + 1) % capacity;
+    arr[rear] = item;
+    count++;
+}
+
+int Queue::peek()
+{
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+    return arr[front];
+}
+
+int Queue::size()
+{
+    return count;
+}
+
+bool Queue::isEmpty()
+{
+    return (size() == 0);
+}
+
+bool Queue::isFull()
+{
+    return (size() == capacity);
+}
+
+struct Stack
+{
+private:
+    int* arr;
+    int top;
+    int capacity;
+public:
+    Stack(int size = SIZE);
+    ~Stack();
+
+    void push(int);
+    int pop();
+    int peek();
+
+    int size();
+    bool isEmpty();
+    bool isFull();
+
+    int* getArr() const { return arr; }
+    int getTop() const { return top; }
+    int getCapacity() const { return capacity; }
+};
+
+Stack::Stack(int size)
+{
+    arr = new int[size];
+    capacity = size;
+    top = -1;
+}
+
+Stack::~Stack()
+{
+    delete[] arr;
+}
+
+void Stack::push(int x)
+{
+    if (isFull())
+    {
+        cout << "Overflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Inserting " << x << endl;
+    arr[++top] = x;
+}
+
+int Stack::pop()
+{
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Removing " << peek() << endl;
+
+    return arr[top--];
+}
+
+int Stack::peek()
+{
+    if (!isEmpty())
+        return arr[top];
+    else
+        exit(EXIT_FAILURE);
+}
+
+int Stack::size()
+{
+    return top + 1;
+}
+
+bool Stack::isEmpty()
+{
+    return top == -1;
+}
+
+bool Stack::isFull()
+{
+    return top == capacity - 1;;
+}
+
+int main()
+{
+    Stack pt(3);
+
+    pt.push(1);
+    pt.push(2);
+
+    pt.pop();
+    pt.pop();
+
+    pt.push(3);
+
+    cout << "The top element is " << pt.peek() << endl;
+    cout << "The stack size is " << pt.size() << endl;
+
+    pt.pop();
+
+    if (pt.isEmpty())
+        cout << "The stack is empty\n";
+    else
+        cout << "The stack is not empty\n";
+
+    cout << endl;
+
+    Queue q(5);
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    cout << "The front element is " << q.peek() << endl;
+    q.dequeue();
+
+    q.enqueue(4);
+
+    cout << "The queue size is " << q.size() << endl;
+
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+
+    if (q.isEmpty())
+        cout << "The queue is empty\n";
+    else
+        cout << "The queue is not empty\n";
+
+    return 0;
+}
